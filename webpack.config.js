553b0dd896435966ad563f899bea4cf2952f7d7c
Anatoly Kobzisty <kobzistyy.a@gmail.com>
2019-09-09 21:11:00 +0600

    Added layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,16 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const StyleLintPlugin = require("stylelint-webpack-plugin");
 
 module.exports = {
   // Входной файл
-  entry: [
-    './src/js/index.js'
-  ],
+  entry: ["./src/js/index.js"],
 
   // Выходной файл
   output: {
-    filename: './js/bundle.js'
+    filename: "./js/bundle.js"
   },
 
   // Source maps для удобства отладки
@@ -22,12 +21,12 @@ module.exports = {
       // Транспилируем js с babel
       {
         test: /\.js$/,
-        include: path.resolve(__dirname, 'src/js'),
+        include: path.resolve(__dirname, "src/js"),
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ["@babel/preset-env"]
           }
         }
       },
@@ -37,10 +36,10 @@ module.exports = {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader, // Extract css to separate file
-          'css-loader', // translates CSS into CommonJS
-          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader", // compiles Sass to CSS, using Node Sass by default
           "postcss-loader" // parse CSS and add vendor prefixes to CSS rules
-        ],
+        ]
       },
 
       // Подключаем шрифты из css
@@ -48,45 +47,58 @@ module.exports = {
         test: /\.(eot|ttf|woff|woff2)$/,
         use: [
           {
-            loader: 'file-loader?name=./fonts/[name].[ext]'
-          },
+            loader: "file-loader?name=./fonts/[name].[ext]"
+          }
         ]
       },
 
       // Подключаем картинки из css
       {
-        test: /\.(svg|png|jpg|jpeg|webp)$/,
+        test: /\.(svg|png|jpg|jpeg|ico|webp)$/,
         use: [
           {
-            loader: 'file-loader?name=./static/[name].[ext]'
-          },
+            loader: "file-loader?name=./static/[name].[ext]"
+          }
         ]
-      },
-    ],
+      }
+    ]
   },
   plugins: [
     // Подключаем файл html, стили и скрипты встроятся автоматически
     new HtmlWebpackPlugin({
-      title: 'Webpack 4 Starter',
-      template: './src/index.html',
+      title: "SCP",
+      template: "./src/index.html",
       inject: true,
       minify: {
         removeComments: true,
-        collapseWhitespace: false,
+        collapseWhitespace: false
       }
     }),
 
     // Кладем стили в отдельный файлик
     new MiniCssExtractPlugin({
-      filename: 'style.css',
+      filename: "style.css"
     }),
 
     // Копируем картинки
     new CopyWebpackPlugin([
       {
-        from: './src/img',
-        to: 'img',
+        from: "./src/favicon",
+        to: "./favicon"
       },
-    ])
-  ],
+      {
+        from: "./src/img",
+        to: "img"
+      }
+    ]),
+
+    new StyleLintPlugin({
+      configFile: ".stylelintrc.json",
+      context: "src",
+      files: "**/*.scss",
+      failOnError: false,
+      quiet: false,
+      emitErrors: true
+    })
+  ]
 };
